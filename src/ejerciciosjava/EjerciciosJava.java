699d96cb4a87c41b78bb8832a5e9fd2d9e385988
Rodrigo Durán <rdurandiez@gmail.com>
2021-12-17 17:48:11 +0100

    Empezamos los ejercicios basicos en java

---
+++ src/ejerciciosjava/EjerciciosJava.java
@@ -0,0 +1,31 @@
+
+package ejerciciosjava;
+
+/**
+ *
+ * @author RODRIGO
+ */
+public class EjerciciosJava {
+
+   public boolean fiestaArdillas(int numBellotas, boolean finDeSemana){
+     //si es fin de semana da igual cuantas bellotas haya
+     if(finDeSemana){
+          return true;
+     }
+      if (40 <= numBellotas && numBellotas <= 60){
+          return true;
+      }
+      return false;
+   }
+   
+
+    public static void main(String[] args) {
+        EjerciciosJava ejercicio = new EjerciciosJava();
+       System.out.println("Ejercicio 01: ARDILLAS");
+        System.out.println( "30,false"+ejercicio.fiestaArdillas(30, false));
+         System.out.println( "50,false"+ ejercicio.fiestaArdillas(50, false));
+          System.out.println(  "70,true"+ejercicio.fiestaArdillas(70, true));
+        
+    }
+    
+}
